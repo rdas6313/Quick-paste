31ed9e0414398c2f0640412ccc0ae83527812258
rdas6313 <rdas6313@gmail.com>
2022-08-28 22:06:45 +0530

    feature: created push to paste data in pastebin

---
+++ pastebin_remote.py
@@ -0,0 +1,181 @@
+from .logger import Logger
+import http.client as http
+from urllib.parse import quote
+import json
+import os
+
+
+class PasteType:
+	PUBLIC = "0"
+	UNLISTED = "1"
+	PRIVATE = "2"
+
+class PasteExpire:
+	NEVER = "N"
+	YEAR = "1Y"
+	HALF_YEAR = "6M"
+	MONTH = "1M"
+	HALF_MONTH = "2W"
+	WEEK = "1W"
+	DAY = "1D"
+	HOUR = "1H"
+	MINUTE = "10M"
+
+class User:
+	GUEST = 0
+	USER = 1
+
+class ConfigType:
+	API_KEY = "api_dev_key"
+	PASTE_URL = "paste_url"
+	BASE_URL = "base_url"
+	GENERAL_ERROR_MSG = "general_error_msg"
+	CLIENT_ERROR_MSG = "client_error_msg"
+	SERVER_ERROR_MSG = "server_error_msg"
+	REDIRECT_ERROR_MSG = "redirect_error_msg"
+	INFO_ERROR_MSG = "info_error_msg"
+
+class Pastebin:
+	API_PASTE_CODE  = "api_paste_code"
+	API_PASTE_PRIVATE = "api_paste_private"
+	API_PASTE_NAME = "api_paste_name"
+	API_PASTE_EXPIRE = "api_paste_expire_date"
+	API_PASTE_FORMAT = "api_paste_format"
+	API_USER_KEY = "api_user_key"
+	API_OPTION = "api_option"
+
+class PastebinRemote:
+
+
+	def __init__(self):
+		self.log = Logger()
+		self.configs = {}
+		self.loadConfig()
+		
+
+	def getConfigPath(self):
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		config_path = os.path.join(dir_path,'config.json')
+		return config_path
+		
+
+	def loadConfig(self):
+		try:
+
+			with open(self.getConfigPath(), 'r') as config_file:
+				config = json.load(config_file)
+				for key in config.keys():
+					self.configs[key] = config[key]
+
+		except FileNotFoundError as e:
+			self.log.error("PastebinRemote: {}".format(e))
+		except:
+			self.log.error("PastebinRemote: Unknown Error")
+
+	def generatePayload(self,data): #data : dict
+		payload = ""
+		try:
+			dataList = list(data.keys())
+			self.log.debug(data)
+			self.log.debug(dataList)
+			for key in dataList:
+				self.log.debug(""+key+":"+data[key])
+				if key is ConfigType.API_KEY or key is Pastebin.API_PASTE_CODE:
+					payload += key + '=' + quote(data[key])
+				else:
+					payload += key + '=' + data[key]	
+				if key != dataList[-1]:
+					payload += '&'
+
+
+			self.log.debug(payload)
+		except:
+			self.log.error("PastebinRemote: Unknown error in generatePayload")
+		finally:
+			return payload
+
+	def push(self,data): #data : dict
+		conn = None
+		res = None
+		try:
+			
+			api_option = "paste"
+			self.log.info("PastebinRemote Connecting to Pastebin server.")
+			conn = http.HTTPSConnection(self.configs[ConfigType.BASE_URL])
+			data[ConfigType.API_KEY] = self.configs[ConfigType.API_KEY]
+			data[Pastebin.API_OPTION] = api_option
+			payload = self.generatePayload(data)
+			self.log.debug(payload)
+			headers = {
+			  'Content-Type': 'application/x-www-form-urlencoded'
+			}			
+			conn.request("POST", self.configs[ConfigType.PASTE_URL], payload, headers)
+			res = conn.getresponse()
+			res_data = res.read()
+			data = res_data.decode("utf-8")
+			self.log.info("PastebinRemote Response Code : {}".format(res.status))
+			self.log.info("PastebinRemote Response Data : {}".format(data))
+			
+
+		except ValueError as e:
+			self.log.error("PastebinRemote: {}".format(e))
+		except InvalidURL as e:
+			self.log.error("PastebinRemote: {}".format(e))
+		except NotConnected as e:
+			self.log.error("PastebinRemote: {}".format(e))
+		except CannotSendRequest as e:
+			self.log.error("PastebinRemote: {}".format(e))
+		except HTTPException as e:
+			self.log.error("PastebinRemote: {}".format(e))
+		except:
+			self.log.error("PastebinRemote: Unknown exception")
+		finally:
+			if conn:
+				conn.close()
+				self.log.info("PastebinRemote Connection is closed with server")
+			else:
+				self.log.error("PastebinRemote: Connection is null")
+			if res:
+				return (res.status,data)
+			else:
+				return (400,None)
+
+
+
+
+
+class PastebinDriver(PastebinRemote):
+
+	def __init__(self):
+		PastebinRemote.__init__(self)
+
+	def guestPush(self,paste_code,callable,paste_name="default"): #paste_code : str , callable : method(tuple) , paste_name : str
+		data = {
+			Pastebin.API_PASTE_CODE : paste_code,
+			Pastebin.API_PASTE_NAME : paste_name,
+			Pastebin.API_PASTE_PRIVATE : PasteType.PUBLIC,
+			Pastebin.API_PASTE_EXPIRE : PasteExpire.NEVER
+		}
+	
+		#add paste format,get format and add it like data[Pastebin.API_PASTE_FORMAT] = ".c"
+		self.log.debug(data)
+		status,msg = self.push(data)
+		res_msg = ""
+		is_success = False
+		if status >= 100 and status < 200:
+			res_msg = self.configs[ConfigType.INFO_ERROR_MSG]
+		elif status >= 300 and status < 400:
+			res_msg = self.configs[ConfigType.REDIRECT_ERROR_MSG]
+		elif status >= 400 and status < 500:
+			res_msg = self.configs[ConfigType.CLIENT_ERROR_MSG]
+		elif status >= 500 and status < 600:
+			res_msg = self.configs[ConfigType.SERVER_ERROR_MSG]
+		else:
+			is_success = True
+		if msg:
+			res_msg += msg
+
+		callable((is_success,res_msg))
+
+
+		
---
+++ tests/test_pastebin_remote.py
@@ -0,0 +1,64 @@
+from unittesting import DeferrableTestCase
+from Quick_Paste.pastebin_remote import *
+
+
+class TestPastebinRemote(DeferrableTestCase):
+
+	def setUp(self):
+		self.remote = PastebinRemote()
+
+	def test_loadConfig(self):
+		self.remote.loadConfig()
+		configs = self.remote.configs
+		key = configs[ConfigType.API_KEY]
+		paste_url = configs[ConfigType.PASTE_URL]
+		base_url = configs[ConfigType.BASE_URL]
+		general_error_msg = configs[ConfigType.GENERAL_ERROR_MSG]
+		self.assertTrue(configs)
+		self.assertEqual(paste_url,"/api/api_post.php")
+		self.assertEqual(key,"AIKXQEl1Z3pZhZO1q7afM7LOrAkZ5uE3")
+		self.assertEqual(base_url,"pastebin.com")
+		self.assertEqual(general_error_msg,"Oops! Some unexpected error happened. \n")
+	
+	def test_getConfigPath(self):
+		path = self.remote.getConfigPath()
+		actual_path = "/home/rdas6313/.config/sublime-text/Packages/Quick_Paste/config.json"
+		self.assertEqual(path,actual_path)
+
+	def test_generatePayload_correct_data(self):
+		data = { "api_dev_key" : "AIKXQEl1Z3pZhZO1q7afM7LOrAkZ5uE3" ,"api_paste_code" : "Generate Paste example" }
+		actual_payload = self.remote.generatePayload(data)
+		payload = "api_dev_key=AIKXQEl1Z3pZhZO1q7afM7LOrAkZ5uE3&api_paste_code=Generate%20Paste%20example"
+		self.assertEqual(actual_payload,payload)
+
+	def test_generatePayload_correct_data_2(self):
+		data = {'api_paste_code': 'sample code', 'api_paste_private': "0", 'api_paste_expire_date': 'N', 'api_paste_name': 'sample paste name'}
+		payload = self.remote.generatePayload(data)
+		result_shoould_be = "api_paste_code=sample%20code&api_paste_private=0&api_paste_expire_date=N&api_paste_name=sample paste name"
+		self.assertEqual(payload,result_shoould_be)
+
+	def test_generatePayload_no_data(self):
+		self.assertRaises(Exception,self.remote.generatePayload(None))
+		#pass
+
+	def test_generatePayload_wrong_type_data(self):
+		data = ["raja","das","abc"]
+		self.assertRaises(Exception,self.remote.generatePayload(data))		
+		#pass
+
+	def test_push_with_correct_data(self):
+		paste_data = { "api_paste_code" : "Paste Test code" }
+		res_code,msg = self.remote.push(paste_data)
+		self.assertEqual(res_code,200)
+
+	def test_push_wrong_data(self):
+		paste_data = { "api_paste_codwest" : "paste_code" }
+		res_code,msg = self.remote.push(paste_data)
+		self.assertNotEqual(res_code,200)
+
+	def test_push_no_data(self):
+		#pass
+		status,msg = self.remote.push(None)
+		self.assertEqual(status,400)
+		self.assertFalse(msg)
+
